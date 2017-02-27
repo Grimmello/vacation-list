d61aa8564dc1b6fd3b8117d569d63d58fe9a365d
Clifford Grimmell <agrimmell2@gmail.com>
2017-02-27 11:01:47 -0800

    add forms and javascript to create list of inputs

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Places(place,landmarks,time) {
+  this.locations = place;
+  this.landmark = landmarks;
+  this.timeOfYear= time;
+}
+
+$(function () {
+  $("form#vacation-place").submit(function(event) {
+    event.preventDefault();
+
+    var placeName = $("input#placeName").val();
+    var placeLand = $("input#placeLandmarks").val();
+    var placeTime = $("input#placeTime").val();
+
+    var newPlace = new Places(placeName, placeLand, placeTime);
+    console.log(newPlace);
+
+    $("ul.location").append("<li><span class='location'>"+ newPlace.locations +"</span></li>");
+
+    $(".location").last().click(function() {
+      $("#show-location").show();
+      $("#locationList h2").text(newPlace.locations);
+      $(".location-name").text(newPlace.locations);
+      $(".nearby-landmarks").text(newPlace.landmark);
+      $(".time-visited").text(newPlace.timeOfYear);
+    });
+  });
+});
